45e6d35e51981aa9274b926e6740e62a1e409579
heymind <heymind@outlook.com>
2019-04-17 22:58:18 +0800

    It has a wonderful syntax for database interaction.

---
+++ src/backend/enpoints/RegisterEndpoint.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 尹思维 on 2019-04-16.
+//
+
+#include "RegisterEndpoint.h"
+
+#include <iostream>
+#include <vector>
+
+bool RegisterEndpoint::handle(RequestMessage &req, std::vector<ResponseMessage> &responses) {
+    BEGIN_HANDLE("REGISTER_PLAYER")
+            NOTIFY("INSERT INTO PLAYERS VALUES(NULL,?,?,0,0,0,0);" << PARAM(0) << PARAM(1));
+        THEN_HANDLE("REGISTER_EXAMINER")
+            NOTIFY("INSERT INTO EXAMINERS VALUES(NULL,?,?,0,0)" << PARAM(0) << PARAM(1));
+    END_HANDLE;
+
+}
+
+RegisterEndpoint::RegisterEndpoint(const sqlite::database &db) : db(db) {}
---
+++ src/backend/enpoints/RegisterEndpoint.h
@@ -0,0 +1,22 @@
+//
+// Created by 尹思维 on 2019-04-16.
+//
+
+#ifndef PROJECT_REGISTERENDPOINT_H
+#define PROJECT_REGISTERENDPOINT_H
+
+#include "../AbstractRequestEndpoint.h"
+
+class RegisterEndpoint : AbstractRequestEndpoint {
+private:
+    sqlite::database db;
+public:
+    bool handle(RequestMessage &req, vector<ResponseMessage> &responses) override;
+
+    RegisterEndpoint(const sqlite::database &db);
+
+
+};
+
+
+#endif //PROJECT_REGISTERENDPOINT_H
